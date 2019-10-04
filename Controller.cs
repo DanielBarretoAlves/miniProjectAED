fc9d2d5032be2573e5fc4b9dbce1f69346bad1db
Daniel Barreto <danielbarreto559@gmail.com>
2019-10-04 08:40:06 -0300

    Method That Calc Days

--- Controller.cs
+++ Controller.cs
@@ -6,8 +6,9 @@ double _saveVal;
 int _payDay;
 int _billsDay;
 int _days = 0;
-int _weeks = 0;
+int _years = 0;
 int _months = 0;
+List<User> userList = new List<User>();
 
 // TODO: Dois Construtores
 public Controller(){
@@ -35,10 +36,53 @@ public Controller(double saveVal, int payDay, int billsDay)
     Console.WriteLine("Salary: ");
     double payment = double.Parse(Console.ReadLine());
 
-    List<User> userList = new List<User>();
+    
     userList.Add(new User(name, budget, payment));
     // Console.WriteLine(userList[0].Name);
     userList[0].addBills();
+    calcTime();
+  }
+
+  // TODO: Calc o Tempo Estimado
+  public void calcTime()
+  {
+    int selectedUser;
+    Console.WriteLine("Selecione um Usuario");
+    for(int i =0; i < userList.Count; i++)
+    {
+      Console.WriteLine(i + " - " + userList[i].Name);
+    }
+    selectedUser = int.Parse(Console.ReadLine());
+    // Cash
+    while(userList[selectedUser].Cash < _saveVal)
+    {
+
+      double _saveVal;
+      // _payDay;
+      // _billsDay;
+      // _days++;
+
+      // PayDay
+      if(_days == _payDay)
+      {
+        // TODO: Call Method Payday
+
+      }
+
+      if(_days == _billsDay)
+      {
+        // TODO: Call MethodBills Day
+      }
+
+      //_months
+      if(_days == 30)
+      {
+        _months ++;
+        _days = 0;
+      }
+
+    }
+
   }
 
 
